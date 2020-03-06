c5a388eea108b2d3d05f25a03e697513aaf94d6e
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2020-03-06 13:33:12 -0600

    Initial commit

---
+++ src/practice/InputStreamWriter.java
@@ -0,0 +1,8 @@
+//package practice;
+//
+//import java.io.Reader;
+//
+//public class InputStreamWriter extends Reader {
+//    public InputStreamWriter(Object p0) {
+//    }
+//}
---
+++ src/practice/ReadEmployeeFile.java
@@ -0,0 +1,21 @@
+//package practice;
+//
+//import java.io.*;
+//import java.nio.file.Path;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//
+//import static java.nio.file.StandardOpenOption.CREATE;
+//
+//
+//public class ReadEmployeeFile {
+//    public static void main(String[]args){
+//        Path file = Paths.get("C:\\Users\\dh146891\\IdeaProjects\\Chapter13\\src\\practice\\Employees.txt");
+//        String s = "";
+//
+//        try{
+//            InputStream  = new BufferedInputStream()(Files.newInputStream()(file, CREATE));
+//            BufferedReader reader = new BufferedReader(new InputStreamWriter(input));
+//        }
+//    }
+//}
