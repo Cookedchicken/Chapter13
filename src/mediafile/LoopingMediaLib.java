9cf125241684a3490e494460df6a368c3e150725
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2020-03-09 13:56:18 -0500

    update

---
+++ src/mediafile/LoopingMediaLib.java
@@ -0,0 +1,12 @@
+package mediafile;
+
+public class LoopingMediaLib {
+    public static void main()
+    {
+        String songInfo = MediaFile.readString();
+        System.out.println(songInfo);
+
+    }
+
+
+}
