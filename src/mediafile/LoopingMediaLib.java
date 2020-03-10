2cc887f565a4b186b0025f586dfab0525050d86e
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2020-03-10 13:59:16 -0500

    update

--- src/mediafile/LoopingMediaLib.java
+++ src/mediafile/LoopingMediaLib.java
@@ -3,8 +3,33 @@ package mediafile;
 public class LoopingMediaLib {
     public static void main()
     {
-        String songInfo = MediaFile.readString();
-        System.out.println(songInfo);
+        String s = MediaFile.readString();
+        while (s != null){
+            System.out.println( "From File " + s);
+            s = MediaFile.readString();
+        }
+        while (s !=null)
+        {
+         String title = s.substring(0, s.indexOf("1"));
+
+            String rating = s.substring(s.indexOf("1") +2);
+            System.out.println("Rating " + rating);
+            System.out.println();
+            s = MediaFile.readString();
+        }
+        while (s !=null)
+        {
+         int rating = Integer.parseInt(s.substring(s.indexOf("1") +2));
+         if()
+         {
+          System.out.println("Rating " + rating);
+          s = MediaFile.readString();
+         }
+        }
+
+
+
+
 
     }
 
---
+++ src/practice/TestingArrays.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class TestingArrays {
+
+    public static void main(String[]args) {
+
+        int[] values = new int[4];
+
+        values[0] = 10;
+        values[1] = 5;
+        values[2] = 21;
+        values[3] = 16;
+
+        int total = 0;
+        int i;
+        for (i = 0; i < values.length; ++i)
+            System.out.println(values[i]);
+        total = total = values[i];
+        System.out.println(total);
+    }
+
+}
